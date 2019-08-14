7f0c8552c10ff38e6885ea9ef7a427da16698281
Andrew Plotnikov <Galean.pal@gmail.com>
2019-08-14 17:45:55 +0300

    Project created

---
+++ RLTestProject/RLTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RLTestProject
+//
+//  Created by Galean Pallerman on 14.08.2019.
+//  Copyright © 2019 GPco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
